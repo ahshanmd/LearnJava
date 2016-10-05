e06000147eb9e0479c3c8590461105c60abbb163
ahshanmd <ahshan.md@gmail.com>
2016-10-06 00:55:22 +0530

    My Initial Git - java code

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package learn.array;
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Welcome to the world of Java and you rock");
+    }
+
+}
